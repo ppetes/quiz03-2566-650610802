0a25e8a250fc8328db8b9de16c2ee4359a67101e
Ratchapon Prangthong <ppete2547@gmail.com>
2023-09-14 15:11:43 +0700

    finish

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -4,62 +4,74 @@ import { nanoid } from "nanoid";
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
+  const roomId = request.nextUrl.searchParams.get("roomId");
   readDB();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  if (!roomId)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
 };
 
 export const POST = async (request) => {
+  const body = await request.json();
+  const { roomId, messageText } = body;
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  if (roomId === DB.rooms.roomId)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found `,
+      },
+      { status: 404 }
+    );
 
   const messageId = nanoid();
+  DB.messages.push({
+    roomId,
+    messageId,
+    messageText,
+  });
 
   writeDB();
 
   return NextResponse.json({
     ok: true,
-    // messageId,
+    messageId,
     message: "Message has been sent",
   });
 };
 
 export const DELETE = async (request) => {
   const payload = checkToken();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  const body = await request.json();
+  const { messageId } = body;
+  role = payload.role;
+  if (!payload && role !== "SUPER_ADMIN")
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
 
   readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Message is not found",
-  //   },
-  //   { status: 404 }
-  // );
+  if (!messageId)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Message is not found",
+      },
+      { status: 404 }
+    );
 
   writeDB();
-
+  DB.messages = DB.messages.filter((x) => x.messageId !== messageId);
   return NextResponse.json({
     ok: true,
     message: "Message has been deleted",
